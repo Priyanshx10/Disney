215c9d11fcbd0815938d43ff3e21a71c80a760d7
Priyansh <PriyanshYadav1012@gmail.com>
2024-01-28 14:36:40 +0530

    Multiple Type Safety and Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,15 +1,26 @@
 import Image from "next/image"
 import Link from "next/link"
+import { ThemeToggler } from "./ThemeToggler"
+import SearchInput from "./SearchInput"
+
 
 function Header() {
   return (
-    <header>
-       <Link href="/"></Link>
-       <Image src="https://links.papareact.com/a943ae" alt="Disney logo"
+    <header className="Fixed w-full z-20 top-0 items-center justify-between flex p-5 bg-gradient-to-t from-grey-200/0 via-gray-900/25 to-gray-900">
+       <Link href="/" className="mr-10"></Link>
+       <Image src="https://links.papareact.com/a943ae" 
+         alt="Disney logo"
          width={120}
          height={100}
-         className=" cursor-pointer invergit"
+         className=" cursor-pointer invert-0 dark:invert"
        />
+
+       <div className="flex space-x-2">
+          <SearchInput />
+          {/*Search Input*/}
+          
+          <ThemeToggler />
+       </div>
     </header>
   )
 }
